989da9d60613ddd4c9c59fef2ce1b5008535abe1
WAcAW9 <wacaw9@gmail.com>
2026-08-24 16:37:08 +0900

    feat: 앱스토어 링크 설정 추가

---
+++ lib/config/links.dart
@@ -0,0 +1,6 @@
+/// 스토어·정책 URL. 출시 전이면 빈 문자열을 유지한다.
+abstract final class StoreLinks {
+  static const appStore =
+      'https://apps.apple.com/kr/app/먹어도돼-역류성-식도염-식단/id6798188018';
+  static const googlePlay = '';
+}
